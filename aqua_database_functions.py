b158959be4a5ed8992fa12e563010ca2044e00c5
HaydenElza <hayden.elza@gmail.com>
2015-05-17 12:01:09 -0500

    Fixed print_orders to print for given date.

--- aqua_database_functions.py
+++ aqua_database_functions.py
@@ -18,13 +18,14 @@ x = db.xact()  # Used for transactions
 
 def print_orders():
 	out_path = "/home/babykitty/Orders_" + datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d") + ".csv"  # Set output path, date is added so that multiple order fulfillments may be placed in same directory.
+	date = '03/16/2015' #datetime.datetime.fromtimestamp(time.time()).strftime("%m/%d/%Y")
 	if os.path.isfile(out_path):
 		print (out_path, "already exists, aborting. If you would like to make a new Orders CSV, move or delete the pre-existing CSV for today's date.")
 		return
 
 	fout = open(out_path,"a")  # Set ouput csv file
-
-	for row in db.prepare('select c.fname, c.lname, od.food_item, od.quantity from order_details od join "order" o on o.order_id = od.order_id join customer c on o.cust_id = c.cust_id ORDER BY fname asc, lname asc, food_item asc;'):
+	sql = """select c.fname, c.lname, od.food_item, od.quantity from order_details od join "order" o on o.order_id = od.order_id join customer c on o.cust_id = c.cust_id where o.delivery_date='"""+date+"""' ORDER BY fname asc, lname asc, food_item asc;"""
+	for row in db.prepare(sql):
 		for column in row:
 			item = str(column) + ","
 			fout.write(item)
@@ -164,3 +165,5 @@ def delivery_sequence():
 	print ("Orders exported to", out_path)
 	fout.close()
 	return
+
+print_orders()
